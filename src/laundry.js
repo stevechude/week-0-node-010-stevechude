d08f39b764122e3f11cea5829805076f64f8965e
stevechude <stevechude@gmail.com>
2022-02-07 22:19:00 -0800

    submit assignment

--- src/laundry.js
+++ src/laundry.js
@@ -8,7 +8,7 @@
  */
  function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
      //declare 2 variables (a maxCount & an empty array to hold values).
-    let maxPair = 0;  
+    let countPair = 0;  
     let singlePair = [];
     //sort through clean & dirty piles in ascending order...
     cleanPile = cleanPile.sort((a, b) => {
@@ -20,7 +20,7 @@
     //loop through cleanPile to find pairs & splice 'em out.
     for (let i = 0; i < cleanPile.length; i++) {
       if (cleanPile[i] === cleanPile[i + 1]) {
-        maxPair++
+        countPair++
         cleanPile.splice(i, 1)
       }
       else {
@@ -33,19 +33,19 @@
     for (let j = 0; j < singlePair.length; j++) {
       if (dirtyPile.includes(singlePair[j]) && noOfWashes > 0) {
         dirtyPile.splice(j, 1);
-        maxPair++
+        countPair++
         noOfWashes--
       }
     }
     //after the 2 previous operations, loop through whats left in d dirtyPIle.
-    //if there's a match, splice it out & also reduce d noOfWashes.
+    //if there's a match, splice it out & also reduce d noOfWashes..
     for (let k = 0; k < dirtyPile.length; k++) {
       if (dirtyPile[k] === dirtyPile[k + 1] && noOfWashes > 1) {
-        maxPair++
+        countPair++
         noOfWashes -= 2
         dirtyPile.splice(k, 1);
       }
     }
-    return maxPair;
+    return countPair;
   }
   module.exports = getMaxPairs;
